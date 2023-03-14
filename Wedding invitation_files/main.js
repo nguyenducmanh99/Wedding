8cdaf8a2fb466e1ad8b18ab6839184d307b369c8
nguyenducmanh99 <72304291+nguyenducmanh99@users.noreply.github.com>
2023-03-14 15:01:23 +0700

    test turn 3

--- Wedding invitation_files/main.js
+++ Wedding invitation_files/main.js
@@ -22,34 +22,39 @@ try {
 
 function timer(clockID, exp_date) {
     // Set the date we're counting down to
-    var countDownDate = new Date(exp_date).getTime();
+    const countDownDate = new Date(exp_date).getTime();
 
     // Update the count down every 1 second
-    var x = setInterval(function () {
+    const x = setInterval(function () {
         // Get today's date and time
-        var now = new Date().getTime();
+        const now = new Date().getTime();
         // Find the distance between now and the count down date
-        var distance = countDownDate - now;
+        const distance = countDownDate - now;
 
         // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
         //add a zero (0) if value less then 9
-        var days = (days < 10) ? '0' + days : days;
-        var hours = (hours < 10) ? '0' + hours : hours;
-        var minutes = (minutes < 10) ? '0' + minutes : minutes;
-        var seconds = (seconds < 10) ? '0' + seconds : seconds;
+        days = (days < 10) ? '0' + days : days;
+        hours = (hours < 10) ? '0' + hours : hours;
+        minutes = (minutes < 10) ? '0' + minutes : minutes;
+        seconds = (seconds < 10) ? '0' + seconds : seconds;
 
         // Output the result in an element with id="demo"
-        // document.getElementById("days").innerHTML = days;
-        // document.getElementById("days").dataset.number = days;
-        document.getElementById("days").textContent = days
-        document.getElementById("hours").textContent = hours;
-        document.getElementById("mins").textContent = minutes;
-        document.getElementById("secs").textContent = seconds;
+        document.getElementById("days").innerText = days
+        document.getElementById("hours").innerText = hours;
+        document.getElementById("mins").innerText = minutes;
+        document.getElementById("secs").innerText = seconds;
+
+        // document.getElementById("days").textContent = days
+        // document.getElementById("hours").textContent = hours;
+        // document.getElementById("mins").textContent = minutes;
+        // document.getElementById("secs").textContent = seconds;
+
+
 
         // If the count down is over, write some text
         if (distance < 0) {
